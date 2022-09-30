828260f1277e444b21e365523048a1b3a883768c
Nathan Clevenger <nateclev@gmail.com>
2022-09-30 12:12:41 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 // import { json } from 'https://pkg.do/apis.do'
 import { json } from 'http://pkg.do/itty-router-extras'
 
-
 export default {
   fetch: req => json(Object.fromEntries(req.headers))
 }
